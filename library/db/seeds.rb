6baa18878d46c2ebc3b438b089493efd2bafa258
xxiem09 <xxiem09@yahoo.com>
2014-10-09 21:24:09 -0400

    Add user record for Users - Jim

--- library/db/seeds.rb
+++ library/db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.where(name:'Ben Lin')
+    .first_or_create!(name:'Ben Lin', user_id:'test1', password_digest:'password1', admin:true)
+User.where(name:'Phil Morgan')
+    .first_or_create!(name:'Phil Morgan', user_id:'test2', password_digest:'password2', admin:false)
+User.where(name:'Jeff Lee')
+	.first_or_create!(name:'Jeff Lee', user_id:'test3', password_digest:'password3', admin:false)
