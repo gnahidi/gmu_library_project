9a7851ae5d3f7c96a3553f86ca080e7ac2fe7aa5
gnahidi <nahidi_ghazaleh@bah.com>
2014-10-14 18:59:36 -0400

    Added the Books link and Book Description

---
+++ library/app/controllers/books_controller.rb
@@ -0,0 +1,51 @@
+class BooksController < ApplicationController
+before_action :set_book, only: [:show, :edit, :update, :destroy]
+
+
+def set_book
+	@book = Book.find(params[:id])
+end
+
+def index
+	@books = Book.order(:title).page(params[:page])
+end
+
+def show
+end
+
+def new
+	@book = Book.new
+end
+
+def create
+	@book = Book.new(book_params)
+	if @book.save
+		redirect_to @book, notice: "#{@book.title} was created!"
+	else
+		render :new
+	end
+end
+
+def edit
+end
+
+def update
+	if @book.update(book_params)
+		redirect_to @book, notice: "#{@book.title} was updated!"
+	else
+		render :new
+	end
+end
+
+def destroy
+	@book.destroy
+	redirect_to books_url
+end
+
+private
+
+def book_params
+	params.require(:book).permit(:title, :genre, :pages, :abstract)
+end
+
+end
--- library/app/models/book.rb
+++ library/app/models/book.rb
@@ -1,3 +1,6 @@
 class Book < ActiveRecord::Base
 	belongs_to :author
+
+	validates :title, :pages, :genre, presence: true
+	
 end
