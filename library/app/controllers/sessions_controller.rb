0777c3a6676856c9b04e1f51e8fe6156d2305237
Alan_Garcia <warlock914@gmail.com>
2014-10-18 00:24:34 -0400

    Mustloginnow_Alan

--- library/app/controllers/sessions_controller.rb
+++ library/app/controllers/sessions_controller.rb
@@ -6,10 +6,10 @@ class SessionsController < ApplicationController
   def create
   	user = User.find_by(user_id: params[:user_id])
   	if user and user.authenticate(params[:password]) and user.admin
-  		session[:user_id] = user.user_id
+  		session[:id] = user.id
   		redirect_to admin_url
   	elsif user and user.authenticate(params[:password])
-      session[:user_id] = user.user_id
+      session[:id] = user.id
       redirect_to users_url
     else
   		redirect_to login_url, alert: "Invalid user/password combination"  		
