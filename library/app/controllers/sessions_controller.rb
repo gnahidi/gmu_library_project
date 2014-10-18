43e4d1ee3461da3e4c27db57e384dbdb3a7acf70
Alan_Garcia <warlock914@gmail.com>
2014-10-17 22:23:14 -0400

    admintoggle-Alan

--- library/app/controllers/sessions_controller.rb
+++ library/app/controllers/sessions_controller.rb
@@ -4,17 +4,20 @@ class SessionsController < ApplicationController
   end
 
   def create
-  	user = User.find_by(name: params[:name])
-  	if user and user.authenticate(params[:password])
+  	user = User.find_by(user_id: params[:user_id])
+  	if user and user.authenticate(params[:password]) and user.admin
   		session[:user_id] = user.user_id
   		redirect_to admin_url
-  	else
+  	elsif user and user.authenticate(params[:password])
+      session[:user_id] = user.user_id
+      redirect_to users_url
+    else
   		redirect_to login_url, alert: "Invalid user/password combination"  		
     end
   end
 
   def destroy
   	session[:user_id] = nil
-  	redirect_to store_url, notice: "Logged out"
+  	redirect_to login_url, notice: "Logged out"
   end
 end
--- library/db/schema.rb
+++ library/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141009235845) do
+ActiveRecord::Schema.define(version: 20141017235922) do
 
   create_table "authors", force: true do |t|
     t.string   "name"
