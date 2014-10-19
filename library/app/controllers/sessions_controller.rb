f079f5a18d7e586f413c599f6382417ea718ee2d
Alan_Garcia <warlock914@gmail.com>
2014-10-19 17:44:56 -0400

    admin_buttons-Alan

--- library/app/controllers/sessions_controller.rb
+++ library/app/controllers/sessions_controller.rb
@@ -7,9 +7,11 @@ class SessionsController < ApplicationController
   	user = User.find_by(user_id: params[:user_id])
   	if user and user.authenticate(params[:password]) and user.admin
   		session[:id] = user.id
+      session[:admin] = true
   		redirect_to books_url
   	elsif user and user.authenticate(params[:password])
       session[:id] = user.id
+      session[:admin] = false
       redirect_to users_url
     else
   		redirect_to login_url, alert: "Invalid user/password combination"  		
