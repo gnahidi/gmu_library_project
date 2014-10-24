9f7a79a1fae7fa5ff4db7a9ed994f876837a62c6
gnahidi <nahidi_ghazaleh@bah.com>
2014-10-23 21:57:54 -0400

    Added Reservation-on and Due-date on feature

--- library/app/models/reservation.rb
+++ library/app/models/reservation.rb
@@ -1,4 +1,12 @@
 class Reservation < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :book
+
+	before_create do |reservation|
+
+		reservation.reserved_on = DateTime.now
+		reservation.due_on = DateTime.now  + 7.days
+	end
+
+	
 end
